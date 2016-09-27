c05e50c75ab3966f3fe3cee43ccfd85a19302ea5
haobug <qingxianhao@gmail.com>
2016-09-28 02:12:15 +0800

    cookie utils (#16)

--- myProject.js
+++ myProject.js
@@ -261,6 +261,48 @@ var genBoundary = function(){
     return "----WebKitFormBoundary" +makeid(16);
 };
 
+/* https://www.perlscriptsjavascripts.com/js/cookies.html */
+function getCookie(w){
+	cName = "";
+	pCOOKIES = new Array();
+	pCOOKIES = document.cookie.split('; ');
+	for(bb = 0; bb < pCOOKIES.length; bb++){
+		NmeVal  = new Array();
+		NmeVal  = pCOOKIES[bb].split('=');
+		if(NmeVal[0] == w){
+			cName = unescape(NmeVal[1]);
+		}
+	}
+	return cName;
+}
+
+function setCookie(name, value, expires, path, domain, secure){
+	cookieStr = name + "=" + escape(value) + "; ";
+
+	if(expires){
+		expires = setExpiration(expires);
+		cookieStr += "expires=" + expires + "; ";
+	}
+	if(path){
+		cookieStr += "path=" + path + "; ";
+	}
+	if(domain){
+		cookieStr += "domain=" + domain + "; ";
+	}
+	if(secure){
+		cookieStr += "secure; ";
+	}
+
+	document.cookie = cookieStr;
+}
+
+function setExpiration(cookieLife){
+    var today = new Date();
+    var expr = new Date(today.getTime() + cookieLife * 24 * 60 * 60 * 1000);
+    return  expr.toGMTString();
+}
+/* https://www.perlscriptsjavascripts.com/js/cookies.html */
+
 var makeFormData = function(fields, boundary){
 //https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/Sending_forms_through_JavaScript
     var crlf = "\r\n";
