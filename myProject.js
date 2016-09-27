20421c99550193909668ccea6679aff120e9f6fb
haobug <qingxianhao@gmail.com>
2016-09-28 02:07:48 +0800

    meta header

--- myProject.js
+++ myProject.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name         another Gmail basic HTML enhancement
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  inspired by GMail Basic HTML Enhancement, more features
+// @author       haobug
+// @match        https://mail.google.com/mail/u/*/h/*
+// @grant        none
+// ==/UserScript==
 /*jshint scripturl:true*/
     var getBaseURL = function(urlstr){
         var link = makeA("getBaseURL", urlstr);
